b945735834b7d220504f8a021798e5cfd93101a0
Abhijeet-Mandrekar <abhijeetmandrekar24@gmail.com>
2024-10-15 11:29:39 +0530

    initial commit

---
+++ flask-app/app.py
@@ -0,0 +1,12 @@
+import os
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return f"<html><body><h1>Flask Deployment Successful for CI/CD</h1></body></html>\n"
+
+@app.route("/env")
+def show_env():
+    env_var = os.getenv('ENV_VARIABLE',None)
+    return f"<html><body><h1>Environment Variable: {env_var}</h1></body></html>\n"
